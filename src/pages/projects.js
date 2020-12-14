61c4e635a999b01660a0acc15c65bb6847d301ef
Ethan Li <lietk12@gmail.com>
2020-12-14 03:24:48 -0800

    Unify projects yml query

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,20 +1,107 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
 
 import SEO from '../components/seo'
 import Layout from '../components/layout'
-import ProjectsPreview from '../components/projects-preview'
-import ProjectsPrototype from '../components/projects-prototype'
+import SectionHeader from '../components/section-header'
 
-const Projects = () => (
-  <Layout breadcrumbs={[{ title: 'projects', path: '/projects' }]}>
-    <SEO title="Projects" description="Ongoing and past projects." />
-    <div className="content">
-      <div className="innerContent">
-        <ProjectsPreview />
-        <ProjectsPrototype />
-      </div>
-    </div>
-  </Layout>
-)
+const Projects = ({ data }) => {
+  const {
+    allProjectsYaml: { edges },
+  } = data
+
+  const projectsPreview = edges.filter(({ node }) => node.status === 'preview')
+  const projectsPrototype = edges.filter(({ node }) => node.status === 'prototype')
+
+  return (
+    <>
+      <Layout breadcrumbs={[{ title: 'projects', path: '/projects' }]}>
+        <SEO title="Projects" description="Ongoing and past projects." />
+        <div className="content">
+          <div className="innerContent">
+            <SectionHeader
+              title="Preview"
+              description="These projects have been announced in papers, preprints,
+              and/or websites. They require significant work in technical development
+              and/or documentation before they will be ready for broad use."
+            />
+            {projectsPreview.map(({ node }) => {
+              const {
+                title,
+                // subtitle,
+                // description,
+                // status,
+                // role,
+                // repos,
+                // preprints,
+                // websites,
+              } = node
+
+              return (
+                <h1>{title}</h1>
+              )
+            })}
+            <SectionHeader
+              title="Prototype"
+              description="These projects are proofs-of-concept and require further development
+              before they will be ready for reuse."
+            />
+            {projectsPrototype.map(({ node }) => {
+              const {
+                title,
+                // subtitle,
+                // description,
+                // status,
+                // role,
+                // repos,
+                // preprints,
+                // websites,
+              } = node
+
+              return (
+                <h1>{title}</h1>
+              )
+            })}
+          </div>
+        </div>
+      </Layout>
+    </>
+  )
+}
+
+Projects.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export const postsQuery = graphql`
+  query {
+    allProjectsYaml {
+      edges {
+        node {
+          title
+          subtitle
+          description
+          status
+          role
+          repos {
+            name
+            url
+          }
+          preprints {
+            description
+            server
+            date
+            url
+          }
+          websites {
+            name
+            url
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Projects
